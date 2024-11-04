d22c41ff593ce183af3ec4099ae9fad2f111bd61
keshuanec <jirkahavlas@gmail.com>
2024-11-04 19:46:25 +0100

    provedena zmena

--- clovicko.py
+++ clovicko.py
@@ -126,3 +126,4 @@ def print_optimal_biases():             #nejlepší bias pro plány o velikosti 
 #print_average_lengths(2, 100)
 #print(analyze_game(42, 100, 0))
 #print(play_game(500, 1, True))
+print("tohle je pokusnej vklad, tak snad to bude fachat")
